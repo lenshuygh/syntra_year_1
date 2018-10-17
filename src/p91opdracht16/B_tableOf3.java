1ab773fb9bac8de0b207f425bbbf7990932906cb
Lens <lens.huygh@gmail.com>
2018-10-17 19:54:58 +0200

    lesson exercises p81 opdracht 16 #2 multiple solutions

--- src/p91opdracht16/B_tableOf3.java
+++ src/p91opdracht16/B_tableOf3.java
@@ -3,9 +3,28 @@ package p91opdracht16;
 public class B_tableOf3 {
     public static void main(String[] args) {
         int num = 0;
-        int times = 1;
+
+
         while((num += 3)  <50){
             System.out.println(num);
         }
+
+
+        /*
+        while(num < 50){
+            num = num + 3;
+            if(num < 50){
+                System.out.println(num);
+            }
+        }
+        */
+
+        /*
+        while(num < 50){
+            if((num += 3) < 50){
+                System.out.println(num);
+            }
+        }
+        */
     }
 }
