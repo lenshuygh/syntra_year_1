248b4aeb41e6d00b1175870375e2a6c3c951000e
Lens Huygh <lens.huygh@gmail.com>
2018-11-18 02:50:03 +0100

    finished problem 6 of projecteuler.net

---
+++ src/extraexercises/ProjectEurlerDotNet/Problem06_SumSquareDifference.java
@@ -0,0 +1,28 @@
+package extraexercises.ProjectEurlerDotNet;
+
+public class Problem06_SumSquareDifference {
+    /*
+        The sum of the squares of the first ten natural numbers is,
+        1² + 2² + ... + 10² = 385
+
+        The square of the sum of the first ten natural numbers is,
+        (1 + 2 + ... + 10)² = 55² = 3025
+
+        Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+        Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+     */
+    public static void main(String[] args) {
+        int sumSquares = 0 ;
+        for (int i = 1; i <= 100; i++) {
+            sumSquares += i*i;
+        }
+
+        int squareSum = 0;
+        for (int i = 1; i <= 100 ; i++) {
+            squareSum += i;
+        }
+        squareSum *= squareSum;
+        System.out.println(squareSum - sumSquares);
+    }
+}
