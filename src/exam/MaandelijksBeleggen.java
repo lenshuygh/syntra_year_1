c1207743a9e394ce53c36d08aff0f4eee2b846e3
Lens <lens.huygh@tradelec.be>
2018-11-19 20:23:31 +0100

    exam 19-11-2018: correction floats & doubles

--- src/exam/MaandelijksBeleggen.java
+++ src/exam/MaandelijksBeleggen.java
@@ -8,10 +8,10 @@ public class MaandelijksBeleggen {
 
         System.out.println("Welcome to our monthly savings calculator!");
         System.out.println("For each montly deposit you will have to pay a deposit fee of 4.5%");
-        System.out.println("You'll pay a monthly maintenance fee of 0.08% on the total amount saved (including the current months deposit");
+        System.out.println("You'll pay a monthly maintenance fee of 0.08% on the total amount saved (including the current months deposit).");
         System.out.println("Each year you'll receive an interest rate of 6%");
         System.out.println("How much would you like to save each month?");
-        double amount = scanner.nextDouble();
+        float amount = scanner.nextFloat();
 
         System.out.println("How many years will you be saving?");
         int years = scanner.nextInt();
@@ -22,31 +22,27 @@ public class MaandelijksBeleggen {
         scanner.close();
     }
 
-    private static double computeTotalAmount(double amount, int years) {
-        final double STARTUP_MULTIPLIER = 0.955F;
-        final double MAINTENANCE_FEE_MULTIPLIER = 0.9992F;
-        final double YEARLY_INTEREST_MULTIPLIER = 1.06F;
+    private static double computeTotalAmount(float amount, int years) {
+        final float STARTUP_MULTIPLIER = 0.955F;
+        final float MAINTENANCE_FEE_MULTIPLIER = 0.9992F;
+        final float YEARLY_INTEREST_MULTIPLIER = 1.06F;
 
         double capitalSaved = 0F;
 
         //find amount of months
         int totalMonths = years * 12;
 
-        // calculate for eacht month until totalMoths
+        // calculate for each month until totalMoths
         for (int i = 1; i <= totalMonths; i++) {
             //add amount minus instapkost
             capitalSaved += amount * STARTUP_MULTIPLIER;
-
             //deduct the onderhoudskost from whole capital
             capitalSaved *=  MAINTENANCE_FEE_MULTIPLIER;
-
             // yearly interst payment if months add up to one year
             if(i % 12 == 0){
                 capitalSaved *= YEARLY_INTEREST_MULTIPLIER;
             }
-            System.out.println(i+" -> "+capitalSaved);
         }
-
         return capitalSaved;
     }
 }
