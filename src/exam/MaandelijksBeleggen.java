eb925be8d99d6cbbbe5aa825c772b1efd962169d
Lens <lens.huygh@tradelec.be>
2018-11-19 20:26:34 +0100

    exam 19-11-2018: correction comments language

--- src/exam/MaandelijksBeleggen.java
+++ src/exam/MaandelijksBeleggen.java
@@ -11,11 +11,11 @@ public class MaandelijksBeleggen {
         System.out.println("You'll pay a monthly maintenance fee of 0.08% on the total amount saved (including the current months deposit).");
         System.out.println("Each year you'll receive an interest rate of 6%");
         System.out.println("How much would you like to save each month?");
+
         float amount = scanner.nextFloat();
-
         System.out.println("How many years will you be saving?");
-        int years = scanner.nextInt();
 
+        int years = scanner.nextInt();
         System.out.println("If we save " + amount + " each month for " + years + " years, at the end of all those years we'll have:");
         System.out.println("€"+computeTotalAmount(amount,years));
 
@@ -26,12 +26,10 @@ public class MaandelijksBeleggen {
         final float STARTUP_MULTIPLIER = 0.955F;
         final float MAINTENANCE_FEE_MULTIPLIER = 0.9992F;
         final float YEARLY_INTEREST_MULTIPLIER = 1.06F;
-
         double capitalSaved = 0F;
 
         //find amount of months
         int totalMonths = years * 12;
-
         // calculate for each month until totalMoths
         for (int i = 1; i <= totalMonths; i++) {
             //add amount minus startup fee
