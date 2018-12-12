cd043547a25247e2ea6265665e4a22a0a574a279
Lens <lens.huygh@tradelec.be>
2018-12-12 19:51:59 +0100

    first exercises on Defining Classes

---
+++ src/p147/graphics/Rectangle.java
@@ -0,0 +1,8 @@
+package p147.graphics;
+
+public class Rectangle {
+    public int heigth;
+    public int width;
+    public int x;
+    public  int y;
+}
---
+++ src/p147/graphics/RectangleApp.java
@@ -0,0 +1,31 @@
+package p147.graphics;
+
+public class RectangleApp {
+    public static void main(String[] args) {
+        System.out.println("This program uses a rectangle");
+
+        Rectangle rect = new Rectangle();
+        rect.heigth = 10;
+        rect.width = 6;
+        rect.x = 2;
+        rect.y = 3;
+
+        System.out.println("h: "+ rect.heigth);
+        System.out.println("w: " + rect.width);
+        System.out.println("x: " + rect.x);
+        System.out.println("y: "+rect.y);
+
+        Rectangle rect2 = new Rectangle();
+        rect2.heigth = 5;
+        rect2.width = 20;
+        rect2.x = 12;
+        rect2.y = 30;
+
+        System.out.println("h: "+  rect2.heigth);
+        System.out.println("w: " + rect2.width);
+        System.out.println("x: " + rect2.x);
+        System.out.println("y: "+  rect2.y);
+
+    }
+}
+
