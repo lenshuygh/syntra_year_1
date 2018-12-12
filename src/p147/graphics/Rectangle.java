64ffa801481c784cd7e8bc4303ec7db1453d3919
Lens <lens.huygh@tradelec.be>
2018-12-12 22:04:37 +0100

    first exercises on Defining Classes: end

--- src/p147/graphics/Rectangle.java
+++ src/p147/graphics/Rectangle.java
@@ -1,17 +1,17 @@
 package p147.graphics;
 
 public class Rectangle {
-    public int heigth;
-    public int width;
-    public int x;
-    public  int y;
+    private int heigth;
+    private int width;
+    private int x;
+    private int y;
 
     public int getHeigth() {
         return heigth;
     }
 
     public void setHeigth(int heigth) {
-        this.heigth = heigth;
+        this.heigth = (heigth < 0) ? heigth * -1 : heigth;
     }
 
     public int getWidth() {
@@ -19,7 +19,8 @@ public class Rectangle {
     }
 
     public void setWidth(int width) {
-        this.width = width;
+
+        this.width = (width < 0) ? width * -1 : width;
     }
 
     public int getX() {
@@ -27,7 +28,7 @@ public class Rectangle {
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.x = (x < 0)? 0 : x;
     }
 
     public int getY() {
@@ -35,12 +36,12 @@ public class Rectangle {
     }
 
     public void setY(int y) {
-        this.y = y;
+        this.y = (y < 0) ? 0 : y;
     }
 
     public void grow(int d){
-        this.setWidth(this.width+d);
-        this.setHeigth(this.heigth+d);
+        this.setWidth( (this.width+d < 0) ? 0 : this.width+d);
+        this.setHeigth( (this.heigth+d < 0 ) ? 0 : this.heigth+d);
     }
 
     public void setPosition(int x,int y){
--- src/p147/graphics/RectangleApp.java
+++ src/p147/graphics/RectangleApp.java
@@ -5,36 +5,49 @@ public class RectangleApp {
         System.out.println("This program uses a rectangle");
 
         Rectangle rect = new Rectangle();
-        rect.heigth = 10;
-        rect.width = 6;
-        rect.x = 2;
-        rect.y = 3;
+        rect.setHeigth(10);
+        rect.setWidth(6);
+        rect.setX(2);
+        rect.setY(3);
 
-        System.out.println("h: "+ rect.heigth);
-        System.out.println("w: " + rect.width);
-        System.out.println("x: " + rect.x);
-        System.out.println("y: "+rect.y);
+        System.out.println("h: "+ rect.getHeigth());
+        System.out.println("w: " + rect.getWidth());
+        System.out.println("x: " + rect.getX());
+        System.out.println("y: "+rect.getY());
 
         Rectangle rect2 = new Rectangle();
-        rect2.heigth = 5;
-        rect2.width = 20;
-        rect2.x = 12;
-        rect2.y = 30;
+        rect2.setHeigth(5);
+        rect2.setWidth(20);
+        rect2.setX(12);
+        rect2.setY(30);
 
-        System.out.println("h: "+  rect2.heigth);
-        System.out.println("w: " + rect2.width);
-        System.out.println("x: " + rect2.x);
-        System.out.println("y: "+  rect2.y);
+        System.out.println();
+        System.out.println("h: "+  rect2.getHeigth());
+        System.out.println("w: " + rect2.getWidth());
+        System.out.println("x: " + rect2.getX());
+        System.out.println("y: "+  rect2.getY());
 
-        rect2.grow(10);
+        rect2.grow(-10);
 
-        System.out.println("h: "+  rect2.heigth);
-        System.out.println("w: " + rect2.width);
-        System.out.println("x: " + rect2.x);
-        System.out.println("y: "+  rect2.y);
+        System.out.println();
+        System.out.println("h: "+  rect2.getHeigth());
+        System.out.println("w: " + rect2.getWidth());
+        System.out.println("x: " + rect2.getX());
+        System.out.println("y: "+  rect2.getY());
 
         System.out.println("area: "+rect2.getArea());
         System.out.println("perimeter: " + rect2.getPerimeter());
+
+
+        Rectangle r3 = new Rectangle();
+        r3.setX(-5);
+        r3.setY(-2);
+        r3.setWidth(-15);
+        r3.setHeigth(-20);
+        System.out.println("h: "+  r3.getHeigth());
+        System.out.println("w: " + r3.getWidth());
+        System.out.println("x: " + r3.getX());
+        System.out.println("y: "+  r3.getY());
 
 
     }
