b863d7cfb961178f5b95004039f85561481037ee
Lens <lens.huygh@tradelec.be>
2018-12-12 21:41:25 +0100

    first exercises on Defining Classes: continued

--- src/p147/graphics/Rectangle.java
+++ src/p147/graphics/Rectangle.java
@@ -5,4 +5,56 @@ public class Rectangle {
     public int width;
     public int x;
     public  int y;
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(int heigth) {
+        this.heigth = heigth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void grow(int d){
+        this.setWidth(this.width+d);
+        this.setHeigth(this.heigth+d);
+    }
+
+    public void setPosition(int x,int y){
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public double getArea(){
+        return width*heigth;
+    }
+
+    public int getPerimeter(){
+        return 2*(width+heigth);
+    }
+
+
 }
--- src/p147/graphics/RectangleApp.java
+++ src/p147/graphics/RectangleApp.java
@@ -26,6 +26,17 @@ public class RectangleApp {
         System.out.println("x: " + rect2.x);
         System.out.println("y: "+  rect2.y);
 
+        rect2.grow(10);
+
+        System.out.println("h: "+  rect2.heigth);
+        System.out.println("w: " + rect2.width);
+        System.out.println("x: " + rect2.x);
+        System.out.println("y: "+  rect2.y);
+
+        System.out.println("area: "+rect2.getArea());
+        System.out.println("perimeter: " + rect2.getPerimeter());
+
+
     }
 }
 
