f79d788e4f870c808ba121f6519e064034dc41ea
Lens Huygh <lens.huygh@gmail.com>
2019-01-16 20:33:09 +0100

    p200 exercise 8 start

---
+++ src/p200opdracht8/graphics/Rectangle.java
@@ -0,0 +1,72 @@
+package p200opdracht8.graphics;
+
+/**
+ * Created by JonathanSyntra on 4/12/2016.
+ */
+public class Rectangle extends Shape {
+    private static int count;
+    public static final int ANGLES;
+    private int height;
+    private int width;
+    public final String description = "Rectangle";
+
+    static {
+        count = 0;
+        ANGLES = 4;
+    }
+
+    public Rectangle() {
+        this(5,5);
+    }
+
+    public Rectangle(int height, int width) {
+        this(1,1,height,width);
+    }
+
+    public Rectangle(int x, int y, int height, int width) {
+        this.setPosition(x,y);
+        this.setHeight(height);
+        this.setWidth(width);
+        count++;
+    }
+
+    public Rectangle(Rectangle rectangle){
+        this(rectangle.getX(),rectangle.getY(),rectangle.getHeight(),rectangle.getWidth());
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = Math.abs(height);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = Math.abs(width);
+        //this.width = width < 0 ? -width : width;
+    }
+
+    @Override
+    public double getArea(){
+        return height * width;
+    }
+
+    @Override
+    public double getPerimeter(){
+        return (height + width) * 2;
+    }
+
+    public void grow(int d){
+        this.setHeight(this.getHeight()+d);
+        this.setWidth(this.getWidth()+d);
+    }
+
+    public static int getCount(){
+        return count;
+    }
+}
