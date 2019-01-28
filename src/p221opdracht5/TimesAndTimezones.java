da6e4c166d47f8f91f9cd6daf23ac8e8b52eacc4
Lens Huygh <lens.huygh@gmail.com>
2019-01-28 21:58:29 +0100

    added more ex dd 28 01 2019

---
+++ src/p221opdracht5/TimesAndTimezones.java
@@ -0,0 +1,16 @@
+package p221opdracht5;
+
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+public class TimesAndTimezones {
+    public static void main(String[] args) {
+        ZonedDateTime nowHere = ZonedDateTime.now();
+        System.out.printf("Current timezone: %s%n%n",nowHere.getZone());
+        System.out.printf("%s%n", ZonedDateTime.now(ZoneId.of("Europe/London")));
+        System.out.printf("%s%n", ZonedDateTime.now(ZoneId.of("Australia/Sydney")));
+        System.out.printf("%s%n", ZonedDateTime.now(ZoneId.of("Australia/Adelaide")));
+        System.out.printf("%s%n", ZonedDateTime.now(ZoneId.of("UTC-4")));
+    }
+}
