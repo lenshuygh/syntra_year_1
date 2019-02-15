5e2333a824d31101f1ce64b4bce44c128455248c
Lens Huygh <lens.huygh@gmail.com>
2019-02-15 16:44:31 +0100

    recapping exercises syntra graphics ex10p203

---
+++ src/recap/poly_ex9_p201/graphics/Circle.java
@@ -0,0 +1,56 @@
+package recap.poly_ex9_p201.graphics;
+
+public class Circle extends Shape {
+    public  final String description = "Circle";
+    private static int count;
+    private int radius;
+    public static final int ANGLES = 0;
+
+    {
+        count = 0;
+    }
+
+    public Circle() {
+        this(5);
+    }
+
+    public Circle(int radius) {
+        this(radius, 0, 0);
+    }
+
+    public Circle(int radius, int x, int y) {
+        this.radius = radius;
+        setPosition(x,y);
+        count++;
+    }
+
+    public Circle(Circle circle) {
+        this(circle.getRadius(), circle.getX(), circle.getY());
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void grow(int d){
+        this.setRadius(this.getRadius()+d);
+    }
+
+    public static int getCount(){
+        return count;
+    }
+}
