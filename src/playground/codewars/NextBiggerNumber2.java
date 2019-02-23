d8bd2eaa5f55e381d10e7a1c5c75ddbb7b6560a2
Lens <lens.huygh@gmail.com>
2019-02-23 01:27:22 +0100

    trying to use permutation for codewars NextBiggestNumber

--- src/playground/codewars/NextBiggerNumber2.java
+++ src/playground/codewars/NextBiggerNumber2.java
@@ -1,22 +1,32 @@
 package playground.codewars;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class NextBiggerNumber {
+public class NextBiggerNumber2 {
 
     public static void main(String[] args) {
         // r: expected:<1101087216> but was:<1101087126>
         // 1101087162
 
         //assertEquals(2071, Kata.nextBiggerNumber(2017));
-        Long l = 1101087162l;
+        //long l = 1101087162l;
+        long l = 144;
         List<String> list = getPermutations(String.valueOf(l).toCharArray());
-        Set<Long> longSet = new TreeSet<>();
-        for (String s : list) {
-            longSet.add(Long.parseLong(s));
+        Set<Long> longSet = list.stream().map(Long::parseLong).collect(Collectors.toSet());
+        //List<Long> longList = list.stream().map(Long::parseLong).collect(Collectors.toList());
+
+        //Set<Long> sSet = new SortedSet<>();
         }
-        System.out.println(((TreeSet<Long>) longSet).higher(l));
-    }
+
+
+        /*java.util.Collections.sort(longSet);
+        int index = longSet.indexOf(l);
+        for (Long aLong : longList) {
+            System.out.println(aLong);
+        }
+        System.out.println(longList.get(index+1));*/
+
 
     public static char[] getRestArray(char[] arr, char element) {
         char[] out = new char[arr.length - 1];
