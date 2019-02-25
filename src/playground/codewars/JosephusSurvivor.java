349af41fa9f0035f25d228b224a0164b2bb6302b
Lens <lens.huygh@gmail.com>
2019-02-26 00:49:23 +0100

    added JosephusSurvivor codewars solution continued

--- src/playground/codewars/JosephusSurvivor.java
+++ src/playground/codewars/JosephusSurvivor.java
@@ -4,34 +4,46 @@ import java.util.*;
 
 public class JosephusSurvivor {
     public static void main(String[] args) {
-        System.out.println(josephusSurvivor(7,3));
+        System.out.println(josephusSurvivor(7, 3));
         //should give 4
-        System.out.println(josephusSurvivor(11,19));
+        System.out.println(josephusSurvivor(11, 19));
         // 10
         System.out.println(josephusSurvivor(100, 1));
         //100
-        Deque<Integer> deque = new LinkedList<>();
-        ((LinkedList<Integer>) deque).add(0,50);
-        ((LinkedList<Integer>) deque).add(1,60);
-        ((LinkedList<Integer>) deque).add(2,70);
-        ((LinkedList<Integer>) deque).add(3,80);
-        ((LinkedList<Integer>) deque).add(4,90);
-        ((LinkedList<Integer>) deque).add(5,50);
-        System.out.println(deque.toString());
-        ((LinkedList<Integer>) deque).remove(2);
-        System.out.println(deque.toString());
-        for (int i = 50; i < 90; i+=10) {
-            System.out.println(((LinkedList<Integer>) deque).indexOf(i));
-        }
     }
 
     public static int josephusSurvivor(final int n, final int k) {
-        for (int i = 1; i <= n; i++) {
-            //System.out.println(i);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i+1;
         }
-        return 0;
+        int j = 1;
+        int removed = 0;
+        while (removed < arr.length - 1) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int i1 : arr) {
+                    System.out.printf("%d",i1);
+                }
+                System.out.println();
+                if (j % 3 == 0) {
+                    arr[i] = 0;
+                    removed++;
+                    j = 0;
+                } else {
+                    if (arr[i] != 0) {
+                        j++;
+                    }
+                }
+            }
+        }
+        for (int i : arr) {
+            if(i != 0){
+                return i;
+            }
+        }
+            return 0;
+        }
     }
-}
 
 /*
 In this kata you have to correctly return who is the "survivor", ie: the last element of a Josephus permutation.
