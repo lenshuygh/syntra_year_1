4efa7c6ff27e467fd2eb93fa910e70ad8b41a9c2
Lens <lens.huygh@gmail.com>
2019-02-28 00:06:51 +0100

    ex2 of streams p 308 : fixed

--- src/p312opdracht4/Ex04.java
+++ src/p312opdracht4/Ex04.java
@@ -3,6 +3,9 @@ package p312opdracht4;
 import p309opdracht3.Geslacht;
 
 
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Ex04 {
@@ -15,37 +18,45 @@ public class Ex04 {
         Stream.of(persons).
                 filter(p -> Geslacht.VROUW.equals(p.getGeslacht())).
                 forEach(System.out::println);
-
+        System.out.println();
         System.out.println("gewicht between 3 and 50");
         Stream.of(persons)
                 .filter(p -> p.getGewicht() < 50)
                 .filter((p -> p.getGewicht() > 3))
                 .forEach(System.out::println);
 
+        System.out.println();
         System.out.println("leeftijd +30 and gewicht +50");
         Stream.of(persons)
                 .filter(p -> p.getLeeftijd() > 30)
                 .filter(p -> p.getGewicht() > 50)
                 .forEach(System.out::println);
 
+        System.out.println();
         System.out.println("only ages");
         Stream.of(persons)
                 .map(Person::getLeeftijd)
                 .forEach(System.out::println);
 
+        System.out.println();
         System.out.println("voornaam and achternaam");
         Stream.of(persons)
                 .map(s -> s.getVoornaam() + " " + s.getAchternaam())
                 .forEach(System.out::println);
 
-        /*System.out.println("Average age");
-        int average = Stream.of(persons)
-                .map(s -> s.getLeeftijd())
-                .max();
-*/
+        System.out.println();
+        System.out.println("Average age");
+        OptionalDouble avg = Stream.of(persons).mapToInt(Person::getLeeftijd).average();
+        System.out.println(avg.getAsDouble());
 
-
+        System.out.println();
         System.out.println("minimum age");
-        System.out.println("maximu age");
+        OptionalInt min = Stream.of(persons).mapToInt(Person::getLeeftijd).min();
+        System.out.println(min.getAsInt());
+
+        System.out.println();
+        System.out.println("maximum age");
+        OptionalInt max = Stream.of(persons).mapToInt(Person::getLeeftijd).max();
+        System.out.println(max.getAsInt());
     }
 }
