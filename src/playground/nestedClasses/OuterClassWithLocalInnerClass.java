1734fecdcfa97426b5340344efe5a5e29595e826
lens <lens.huygh@gmail.com>
2019-03-02 16:27:41 +0100

    stated recap local inner classes

---
+++ src/playground/nestedClasses/OuterClassWithLocalInnerClass.java
@@ -0,0 +1,11 @@
+package playground.nestedClasses;
+
+public class OuterClassWithLocalInnerClass {
+    final int CONST = 8;
+    int var = 5;
+    String id = "eggg";
+
+    class LocalInnerClass{
+
+    }
+}
