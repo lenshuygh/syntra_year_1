3275da0d9be4014d1c523ad5a2f008abd480993f
lens <lens.huygh@gmail.com>
2019-03-02 16:03:31 +0100

    inner classes ex01p237

---
+++ src/playground/nestedClasses/p237ex01/Musician.java
@@ -0,0 +1,13 @@
+package playground.nestedClasses.p237ex01;
+
+public class Musician {
+    public void play(){
+        Instrument instrument = new Instrument();
+        instrument.makeSound();
+    }
+    class Instrument{
+        public void makeSound(){
+            System.out.println("Instrument class -> makeSound()");
+        }
+    }
+}
---
+++ src/playground/nestedClasses/p237ex01/MusicianApp.java
@@ -0,0 +1,11 @@
+package playground.nestedClasses.p237ex01;
+
+public class MusicianApp {
+    public static void main(String[] args) {
+        new Musician().play();
+
+        Musician musician = new Musician();
+        Musician.Instrument musicianInstrument = musician.new Instrument();
+        musicianInstrument.makeSound();
+    }
+}
