fe9567a969d454ad6abf9865161fe4abbf7d30b0
Lens Huygh <lens.huygh@gmail.com>
2019-03-12 11:49:33 +0100

    codewars: shortest word kata -> to oneliner

--- src/playground/codewars/ShortestWord.java
+++ src/playground/codewars/ShortestWord.java
@@ -4,11 +4,10 @@ import java.util.stream.Stream;
 
 public class ShortestWord {
     public static void main(String[] args) {
-        System.out.println(findShort("dit is een zin met woorden en da is't."));
+        System.out.println(findShort("dit is a zin met woorden en da is't."));
     }
 
     public static int findShort(String s) {
-        String[] words = s.split(" ");
-        return Stream.of(words).mapToInt(String::length).min().getAsInt();
+        return Stream.of(s.split(" ")).mapToInt(String::length).min().getAsInt();
     }
 }
