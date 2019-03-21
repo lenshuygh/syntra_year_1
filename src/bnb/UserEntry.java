04692bb806eb91ab882206ce8661fc391ca88772
lens <lens.huygh@gmail.com>
2019-03-21 22:00:57 +0100

    syntra: bnb exercise: refactored UserEntry class

--- src/bnb/UserEntry.java
+++ src/bnb/UserEntry.java
@@ -8,24 +8,58 @@ import java.util.Scanner;
 import static bnb.BnbCommands.*;
 
 public class UserEntry {
+    private static final String OVERVIEW_MAIN_CHOICES =
+            "Please choose on of the following commands:%n" +
+                    "1.  Check room availability for a given period.%n" +
+                    "2.  Check reservations by client.%n" +
+                    "3.  Book a reservation.%n" +
+                    "4.  Change a reservation.%n" +
+                    "5.  Print reservation data.%n" +
+                    "0.  Quit Program.%n";
+
+    private static final String QUESTION_STARTING_DATE = "Please provide the starting date of reservation (DD/MM/YYYY): ";
+    private static final String QUESTION_ENTER_ACTION_NUMBER = "Enter the number of the action u want to perform: ";
+
+    private static final String ENTRY_ERR_NUMBER =
+            "---------------------.%n" +
+                    "Please enter a number.%n" +
+                    "---------------------.%n";
+    private static final String ENTRY_ERR_NUMBER_RANGE_0_5 =
+            "----------------------------------%n" +
+                    "Please enter a number from 0 to 5.%n" +
+                    "----------------------------------%n";
+    private static final String ENTRY_ERR_DATE_FORMAT =
+            "------------------------------------------------%n" +
+                    "Please format your date correctly eg: 06/12/2020%n" +
+                    "------------------------------------------------%n";
+
+    private static final String LINE_FEED = "%n";
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static LocalDate formattedDate = null;
+
+    private static String getNextInput(String questionString) {
+        Scanner scanner = new Scanner(System.in);
+        display(questionString);
+        return scanner.next();
+    }
+
     public static BnbCommands getMenuEntry() {
-        printCommandChoices();
-        String stringInput = "";
+        display(OVERVIEW_MAIN_CHOICES);
         int commandChoice = -1;
         boolean goodEntry = false;
-        Scanner scanner = new Scanner(System.in);
 
         while (!goodEntry) {
-            System.out.print("Enter the number of the action u want to perform:");
-            stringInput = scanner.next();
+            String stringInput = getNextInput(QUESTION_ENTER_ACTION_NUMBER);
             try {
                 commandChoice = Integer.parseInt(stringInput);
             } catch (NumberFormatException e) {
-                System.out.println("Please enter a number.");
+                display(ENTRY_ERR_NUMBER);
                 continue;
             }
-            if ((commandChoice < 0) || (commandChoice > 5)) {
-                System.out.println("Please enter a number from 0 to 5.");
+            if (!checkInRange(commandChoice,0,5)) {
+                display(ENTRY_ERR_NUMBER_RANGE_0_5);
+                display(OVERVIEW_MAIN_CHOICES);
             } else {
                 goodEntry = true;
             }
@@ -46,37 +80,28 @@ public class UserEntry {
         }
     }
 
-    public static void printCommandChoices() {
-        System.out.println("Please choose on of the following commands:");
-        System.out.println("1.  Check room availability for a given period.");
-        System.out.println("2.  Check reservations by client.");
-        System.out.println("3.  Book a reservation.");
-        System.out.println("4.  Change a reservation.");
-        System.out.println("5.  Print reservation data.");
-        System.out.println("0.  Quit Program.");
+    private static boolean checkInRange(int numberEntered, int lowLimit, int highLimit) {
+        return (numberEntered >= lowLimit) && (numberEntered <= highLimit);
     }
 
-    public static LocalDate getFromDate(){
-        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        Scanner scanner = new Scanner(System.in);
-        String date = "";
-        LocalDate formattedDate = LocalDate.now();
+    public static LocalDate getFromDate() {
         boolean dateOk = false;
 
-
-        while(!dateOk) {
-            System.out.print("Starting date of reservation (DD/MM/YYYY): ");
-            date = scanner.next();
+        while (!dateOk) {
+            String date = getNextInput(QUESTION_STARTING_DATE);
             try {
-                formattedDate = LocalDate.parse(date, myFormatter);
+                formattedDate = LocalDate.parse(date, DATE_TIME_FORMATTER);
             } catch (DateTimeParseException e) {
-                System.out.println("Please format your date correctly eg: 06/12/2020");
-                System.out.println();
+                display(ENTRY_ERR_DATE_FORMAT);
+                display(LINE_FEED);
                 continue;
             }
             dateOk = true;
         }
-
         return formattedDate;
+    }
+
+    private static void display(String s) {
+        System.out.printf(s);
     }
 }
