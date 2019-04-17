5e6f756b77901f135eb441019e971c82d4441489
lens <lens.huygh@gmail.com>
2019-04-17 21:09:52 +0200

    codewars: added mostFrequentUsedWordInAText ctd

--- src/playground/codewars/MostFrequentUsedWordInAText.java
+++ src/playground/codewars/MostFrequentUsedWordInAText.java
@@ -1,35 +1,71 @@
 package playground.codewars;
 
 import java.nio.CharBuffer;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MostFrequentUsedWordInAText {
+    private static Map<String,Integer> occurencesMap = new HashMap<>();
+    private static List<Integer> sorter = new ArrayList<>();
     public static void main(String[] args) {
+        //String s = "a a a  b  c c  d d d d  e e e e e";
         String s = "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e";
+        //String s = "  , e   .. ";
+        //String s = "  //wont won't won't ";
         System.out.println(top3(s));
     }
+
     public static List<String> top3(String s) {
-        ///only A-Z ascii
-        char[] cArr = s.toCharArray();
+        s = s.replace("  "," ");
+        char[] cArr = s.toLowerCase().toCharArray();
+
 
         IntStream stream = CharBuffer.wrap(cArr).chars();
         String string = stream
-                .filter(c -> ((c >= 'a' && c <= 'z') || (c == '\'') || (c == ' ') || (c >= 'A' && c <= 'Z')))
+                .filter(c -> ((c >= 'a' && c <= 'z') || (c == '\'') || (c == ' ')))
                 .collect(StringBuilder::new,StringBuilder::appendCodePoint,StringBuilder::append).toString();
         System.out.println("=< " + s);
         System.out.println("=> " + string);
 
+        string = string.strip();
 
-        /*Stream.of(s.split(" "))
-                .map(t -> t.replace(",",""))
-                .map(t -> t.replace(".",""))
-                .forEach(System.out::println);*/
-        return null;
+        Stream.of(string.split(" "))
+                .forEach(st -> evaluate(st));
+
+        occurencesMap.forEach((k,v)-> System.out.println(k +" - "+v));
+
+        occurencesMap.values().forEach(sorter::add);
+        sorter.sort(Comparator.reverseOrder());
+        System.out.println("sorter size = " + sorter.size());
+        List<String> outList = new ArrayList<>();
+        for (int i = 0; i < 3 && i < sorter.size(); i++) {
+            System.out.println("i -> " + i);
+
+            String foundString = "";
+            for (String s1 : occurencesMap.keySet()) {
+                Integer searchedNumber = occurencesMap.get(s1);
+                if(searchedNumber == sorter.get(i)){
+                    foundString = s1;
+                    System.out.println("foundString -> " + foundString);
+                    break;
+                }
+            }
+            outList.add(i,foundString);
+        }
+        //occurencesMap = new HashMap<>();
+        occurencesMap.clear();
+        return outList;
+    }
+
+    private static void evaluate(String st) {
+        int count = 0;
+        if(occurencesMap.containsKey(st)){
+            count = occurencesMap.get(st) + 1;
+            occurencesMap.put(st,count);
+        }else{
+            occurencesMap.put(st,1);
+        }
     }
 }
 
