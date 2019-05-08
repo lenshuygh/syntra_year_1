98b14caa981def5e9cb5caefa6b7ff27f4459249
Lens Huygh <lens.huygh@gmail.com>
2019-05-08 22:11:31 +0200

    syntra: fx app p.435

--- src/book_exercises/p435opdracht1/HelloWorldApp.java
+++ src/book_exercises/p435opdracht1/HelloWorldApp.java
@@ -1,4 +1,24 @@
 package book_exercises.p435opdracht1;
 
-public class HelloWorldApp {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class HelloWorldApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label helloLabel = new Label("Hello World");
+        helloLabel.setLayoutX(20);
+        helloLabel.setLayoutY(20);
+        Group rootGroup = new Group(helloLabel);
+        Scene scene = new Scene(rootGroup,300,100);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
